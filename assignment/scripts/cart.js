5b9409afba2b3050389785f95724ca497b2502da
Ben Lynch <bdlynch7@gmail.com>
2023-06-28 04:40:49 -0500

    addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,9 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item) {
+    console.log('in addItem');
+    basket.push(item);
+    console.log(basket);
+    return true;
+}
 
+function listItems() {
+    console.log('in listItems');
+    for (item of basket) {
+        console.log('Items in basket', item);
+    }
+}
 
+function empty() {
+    console.log('in empty');
+    basket = [];
+    console.log(basket);
+}
 
 
 
